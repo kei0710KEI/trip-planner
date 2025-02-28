3bcd08ae283d024ee2ff7c9b0601d11ee4ab5645
kei0710KEI <keisuke0710.taka@gmail.com>
2025-03-01 00:12:08 +0900

    done

---
+++ constant/constant.ts
@@ -0,0 +1,33 @@
+export const navLinks = [
+    {
+        id:1,
+        url:"0",
+        label:"Home"
+    },
+    {
+        id:2,
+        url:"0",
+        label:"About"
+    },
+    {
+        id:3,
+        url:"0",
+        label:"Destination"
+    },
+    {
+        id:4,
+        url:"0",
+        label:"Blog"
+    },
+    {
+        id:5,
+        url:"0",
+        label:"Dashboard"
+    },
+    {
+        id:6,
+        url:"0",
+        label:"Contact"
+    },
+    
+]
